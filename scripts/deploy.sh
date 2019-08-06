98a9b36a3b37d5181ff30fd914593d3d25667ecf
Dzmitry Tsebruk <mikhama@tut.by>
2019-08-06 15:43:44 +0300

    fix 111

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-openssl aes-256-cbc \
-  -K $ENC_KEY \
-  -iv $ENC_IV \
-  -in deploy-key.enc \
-  -out deploy-key \
-  -d
+openssl aes-256-cbc -K $encrypted_f054ef30fcc5_key -iv $encrypted_f054ef30fcc5_iv -in deploy_key.enc -out deploy_key -d
 
-rm deploy-key.enc
-chmod 600 deploy-key
-mv deploy-key ~/.ssh/id_rsa
+rm deploy_key.enc
+chmod 600 deploy_key
+mv deploy_key ~/.ssh/id_rsa
 
 scp ./dist/rs-bumblebee.js "$USERNAME@$SERVER_ADDRESS:/home/$USERNAME/rs-bumblebee/"
 ssh "$USERNAME@$SERVER_ADDRESS" ./rs-bumblebee/run.sh
