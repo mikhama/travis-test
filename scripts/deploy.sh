2cd3d9de9ef01a43b43ab3f64248396ef8cffaaa
Dzmitry Tsebruk <mikhama@tut.by>
2019-08-06 16:02:42 +0300

    i hate this

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-openssl -aes-256-cbc -K $encrypted_f054ef30fcc5_key -iv $encrypted_f054ef30fcc5_iv -in deploy_key.enc -out deploy_key -d &&
+set -x
 
-rm deploy_key.enc &&
-chmod 600 deploy_key &&
-mv deploy_key ~/.ssh/id_rsa &&
+openssl aes-256-cbc -K $encrypted_f054ef30fcc5_key -iv $encrypted_f054ef30fcc5_iv -in deploy_key.enc -out deploy_key -d
 
-scp ./dist/rs-bumblebee.js "$USERNAME@$SERVER_ADDRESS:/home/$USERNAME/rs-bumblebee/" &&
+rm deploy_key.enc
+chmod 600 deploy_key
+mv deploy_key ~/.ssh/id_rsa
+
+scp ./dist/rs-bumblebee.js "$USERNAME@$SERVER_ADDRESS:/home/$USERNAME/rs-bumblebee/"
 ssh "$USERNAME@$SERVER_ADDRESS" ./rs-bumblebee/run.sh
 
 exit
