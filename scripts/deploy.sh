8be3c600929b4c1baf00f56b50859931be255e0c
Dzmitry Tsebruk <mikhama@tut.by>
2019-08-06 15:51:14 +0300

    next hotfix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_f054ef30fcc5_key -iv $encrypted_f054ef30fcc5_iv -in deploy_key.enc -out deploy_key -d
+openssl -aes-256-cbc -K $encrypted_f054ef30fcc5_key -iv $encrypted_f054ef30fcc5_iv -in deploy_key.enc -out deploy_key -d &&
 
-rm deploy_key.enc
-chmod 600 deploy_key
-mv deploy_key ~/.ssh/id_rsa
+rm deploy_key.enc &&
+chmod 600 deploy_key &&
+mv deploy_key ~/.ssh/id_rsa &&
 
-scp ./dist/rs-bumblebee.js "$USERNAME@$SERVER_ADDRESS:/home/$USERNAME/rs-bumblebee/"
+scp ./dist/rs-bumblebee.js "$USERNAME@$SERVER_ADDRESS:/home/$USERNAME/rs-bumblebee/" &&
 ssh "$USERNAME@$SERVER_ADDRESS" ./rs-bumblebee/run.sh
 
 exit
